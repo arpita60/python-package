6f697f00e888926e87ca8a3b5428cf4f55bab969
ap15625 <arpitapattanayak48@gmail.com>
2023-07-25 16:40:01 +0530

    Initial commit

---
+++ Custom Dropdown/build/lib/customDropdown.py
@@ -0,0 +1,4 @@
+from dash import Dash, html, dcc, callback, Output, Input
+
+def customDropdown():
+    print('hello from package')
---
+++ Custom Dropdown/customDropdown/src/customDropdown.py
@@ -0,0 +1,7 @@
+from dash import Dash, html, dcc, callback, Output, Input
+
+def customDropdown():
+    print('hello from package')
+
+# class dropdownCustom(id):
+
